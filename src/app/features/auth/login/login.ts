7703f78f49ea0adead32befb297e4635a57df18f
Vysakh P <vysakhp17@gmail.com>
2025-10-26 19:57:57 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { FloatLabel } from 'primeng/floatlabel';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, InputTextModule, FloatLabel],
+  imports: [FormsModule, InputTextModule, FloatLabel, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { FloatLabel } from "primeng/floatlabel";
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, InputTextModule, FloatLabel, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
