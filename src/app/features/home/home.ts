7bf0744de00c40de2736d5365e23a2069357fcbc
Vysakh P <vysakhp17@gmail.com>
2025-10-26 18:47:25 +0530

    feat(home): add router links to login and register buttons

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Content } from "./content/content";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Content],
+  imports: [Content, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
